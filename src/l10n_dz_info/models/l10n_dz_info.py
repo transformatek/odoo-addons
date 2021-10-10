7e7c7e721f60f202e0b704fcb323a9065f3e1e37
afaysalk <91470600+afaysalk@users.noreply.github.com>
2021-10-10 17:24:18 +0200

    Add files via upload
    Ajout du addon l10n_dz_info

---
+++ src/l10n_dz_info/models/l10n_dz_info.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+#
+# Copyright (c) 2016  - Osis - www.osis-dz.net
+
+from odoo import api ,fields, models
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    rc = fields.Char(string='R.C')
+    nif = fields.Char(string='N.I.F', size=15)
+    nis = fields.Char(string='N.I.S')
+    ai = fields.Char(string='Article d\'imposition')
+    
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    rc = fields.Char(string='R.C')
+    nif = fields.Char(string='N.I.F', size=15)
+    nis = fields.Char(string='N.I.S')
+    ai = fields.Char(string='Article d\'imposition')
